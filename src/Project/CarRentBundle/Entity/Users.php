99524fe32678284cf6c64ece6d5a6356914db1b5
Duong NguyenTuan <tommynt@outlook.com>
2016-02-03 01:19:42 +0100

    logowanieIrejestracja

--- src/Project/CarRentBundle/Entity/Users.php
+++ src/Project/CarRentBundle/Entity/Users.php
@@ -4,13 +4,12 @@ namespace Project\CarRentBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
 * @ORM\Entity
 * @ORM\Table(name="fos_user")
 */
-class Users extends BaseUser implements UserInterface
+class Users extends BaseUser
 {
 /**
 * @ORM\Id
